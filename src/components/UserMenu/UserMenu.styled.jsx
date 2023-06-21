afca0af874ffa18a370142709a3f04088c08f63c
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-06-21 18:02:39 +0300

    fix greeting

--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -14,4 +14,18 @@ export const Greeting = styled.p`
 
   color: #000;
   font-weight: 500;
+
+  @media screen and (max-width: 454px) {
+      position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+
+  white-space: nowrap;
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+  }
 `;
